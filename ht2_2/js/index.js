569e85969e2513523f692b56f624ea56dd6d59f3
Evgeniy Zelenkov <skyydail@gmail.com>
2016-11-10 00:25:10 +0300

    solve - step #1

---
+++ ht2_2/js/index.js
@@ -0,0 +1,39 @@
+(() => {
+    'use strict';
+
+    let data = [1,2,3,4,5];
+
+    let isSomeTrue = (source, filterFn) => {
+
+    };
+
+
+
+})();
+
+// Написать фукнцию 'isSomeTrue', которая принимает 2 параметра - 'source' и 'filterFn'
+//
+// 'source' - массив
+//
+// 'filterFn' - фильтрующая функция
+//
+// Если фильтрующая функция вернет 'true' хотя бы для одного элемента массива, то и сама 'isSomeTrue' вернет 'true'
+//
+// Если фильтрующая функция вернет 'false' для ВСЕХ элементов массива, то и сама 'isSomeTrue' вернет 'false'
+//
+//
+//
+// Всё должно быть реализовано с использованием чистого js (не используя сторонние библиотеки).
+//
+// Так же, нельзя использовать методы для работы с массивами.
+//
+//
+//
+//     пример:
+//
+// console.log(isSomeTrue(allNumbers, isNumber)); //вернет true
+//
+// console.log(isSomeTrue(someNumbers, isNumber)); //вернет true
+//
+// console.log(isSomeTrue(noNumbers, isNumber)); //вернет false
+
