888d5aecf9b32302ff3708b8914fee114ccc5b8b
Evgeniy Zelenkov <skyydail@gmail.com>
2016-11-10 13:17:07 +0300

    second task - complete

--- ht2_2/js/index.js
+++ ht2_2/js/index.js
@@ -1,39 +1,30 @@
 (() => {
     'use strict';
 
-    let data = [1,2,3,4,5];
+    let data = [1,2,3,4,5],
+        newArr = [];
+
+    let falseChecker = value => {
+      if (typeof value === 'string') {
+          return true;
+      } else {
+          newArr.push(value);
+      }
+    };
 
     let isSomeTrue = (source, filterFn) => {
+        for (let i = 0; i < source.length; i++) {
+            filterFn(source[i]);
+        }
+
+        if (source.length === newArr.length) {
+            return false;
+        } else {
+            return true;
+        }
 
     };
 
-
-
+    console.log(isSomeTrue(data,falseChecker));
 })();
-
-// Написать фукнцию 'isSomeTrue', которая принимает 2 параметра - 'source' и 'filterFn'
-//
-// 'source' - массив
-//
-// 'filterFn' - фильтрующая функция
-//
-// Если фильтрующая функция вернет 'true' хотя бы для одного элемента массива, то и сама 'isSomeTrue' вернет 'true'
-//
-// Если фильтрующая функция вернет 'false' для ВСЕХ элементов массива, то и сама 'isSomeTrue' вернет 'false'
-//
-//
-//
-// Всё должно быть реализовано с использованием чистого js (не используя сторонние библиотеки).
-//
-// Так же, нельзя использовать методы для работы с массивами.
-//
-//
-//
-//     пример:
-//
-// console.log(isSomeTrue(allNumbers, isNumber)); //вернет true
-//
-// console.log(isSomeTrue(someNumbers, isNumber)); //вернет true
-//
-// console.log(isSomeTrue(noNumbers, isNumber)); //вернет false
 
