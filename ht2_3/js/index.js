f4f0729c2f80b9da98eb1c1ad75e83c1c8729046
Evgeniy Zelenkov <skyydail@gmail.com>
2016-11-12 22:46:42 +0300

    i think it's true

--- ht2_3/js/index.js
+++ ht2_3/js/index.js
@@ -2,42 +2,85 @@
     let calculator = firstNumber => {
         return {
             sum() {
+                let result = firstNumber;
+
                 for (let i = 0; i < arguments.length; i++) {
-                    firstNumber+=arguments[i];
+                    result+=arguments[i];
                 }
-                return firstNumber;
+                return result;
             },
             diff() {
+                let result = firstNumber;
+
                 for (let j = 0; j < arguments.length; j++) {
-                    firstNumber-=arguments[j];
+                    result-=arguments[j];
                 }
-                return firstNumber;
+                return result;
             },
             div() {
+                let result = firstNumber;
+
                 for (let k = 0; k < arguments.length; k++) {
                     if (!arguments[k]) {
                         throw new Error('Давайте не будем так делать, введите что-нибудь отличное от нуля!')
                     }
-                    firstNumber/=arguments[k];
+                    result/=arguments[k];
                 }
 
-                return firstNumber;
+                return result;
             },
             mul() {
+                let result = firstNumber;
+
                 for (let l = 0; l < arguments.length; l++) {
-                    firstNumber*=arguments[l];
+                    result*=arguments[l];
                 }
 
-                return firstNumber;
+                return result;
             }
         };
     };
+    let myCalculator = calculator(100);
     try {
-        console.log(calculator(100).sum(1, 2, 3));
-        console.log(calculator(100).diff(10, 20));
-        console.log(calculator(100).div(2, 0));
-        console.log(calculator(100).mul(2, 2));
+        console.log(myCalculator.sum(1, 2, 3));
+        console.log(myCalculator.diff(10, 20));
+        console.log(myCalculator.div(2, 2));
+        console.log(myCalculator.mul(2, 2));
     } catch (e) {
         console.log(e.message);
     }
 })();
+
+// Написать функцию 'calculator', которая имеет один параметр - 'result'
+//
+// 'firstNumber' - это число, с которым будут производиться действия
+//
+// Функция 'calculator' должна возвращать объект, у которого должно быть несколько методов.
+//
+//     Каждая из этих функций принимает неограниченное количество аргументов и производит какие-то арифметические операции с этими аргументами и тем числом, которое было передано в 'calculator' и возвращает результат:
+//
+//     - 'sum' - складывает 'firstNumber' с переданным аргументами
+//
+// - 'dif' - вычитает из 'firstNumber' переданные аргументы
+//
+// - 'div' - делит 'firstNumber' на первый переданный аргумент. Результат этой операции делится на второй переданный аргумент (если он есть) и так далее
+//
+// - 'mul' - умножает 'firstNumber' на первый переданный аргумент. Результат этой операции умножается на второй переданный аргумент (если он есть) и так далее.
+//
+//     Предусмотреть исключительные ситуации, для функции 'div', когда делитель равен нулю
+//
+//
+//
+// пример:
+//
+//     var myCalculator = calculator(100);
+//
+//
+//
+// console.log(myCalculator.sum(1, 2, 3)); //вернет 106
+//
+// console.log(myCalculator.dif(10, 20)); //вернет 70
+//
+// console.log(myCalculator.div(2, 2)); //вернет 25
+//
+// console.log(myCalculator.mul(2, 2)); //вернет 400
