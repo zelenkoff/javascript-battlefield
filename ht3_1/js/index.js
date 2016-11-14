87bcbd9b05a649e82c1c85b126263556157d657f
Evgeniy Zelenkov <skyydail@gmail.com>
2016-11-14 18:11:35 +0300

    ht3_1 - done

--- ht3_1/js/index.js
+++ ht3_1/js/index.js
@@ -29,145 +29,215 @@
     //Моя версия map
     let myMap = (arr, callback) => {
 
+        let A = new Array(arr.length);
+
         if (typeof callback !== 'function') {
             throw new Error(`${callback} это не функция!`);
         }
 
-        let A = [];
-
         for (let i = 0; i < arr.length; i++) {
-            callback(arr[i], i, arr);
+            A[i] = callback(arr[i], i, arr);
         }
+
+        return A;
     };
 
-    //Моя версия map - end
-// Шаги алгоритма ECMA-262, 5-е издание, 15.4.4.19
-// Ссылка (en): http://es5.github.com/#x15.4.4.19
-// Ссылка (ru): http://es5.javascript.ru/x15.4.html#x15.4.4.19
-    if (!Array.prototype.map) {
-
-        Array.prototype.map = function(callback, thisArg) {
-
-            var T, A, k;
-
-            if (this == null) {
-                throw new TypeError(' this is null or not defined');
-            }
-
-            // 1. Положим O равным результату вызова ToObject с передачей ему
-            //    значения |this| в качестве аргумента.
-            var O = Object(this);
-
-            // 2. Положим lenValue равным результату вызова внутреннего метода Get
-            //    объекта O с аргументом "length".
-            // 3. Положим len равным ToUint32(lenValue).
-            var len = O.length >>> 0;
-
-            // 4. Если вызов IsCallable(callback) равен false, выкидываем исключение TypeError.
-            // Смотрите (en): http://es5.github.com/#x9.11
-            // Смотрите (ru): http://es5.javascript.ru/x9.html#x9.11
-            if (typeof callback !== 'function') {
-                throw new TypeError(callback + ' is not a function');
-            }
-
-            // 5. Если thisArg присутствует, положим T равным thisArg; иначе положим T равным undefined.
-            if (arguments.length > 1) {
-                T = thisArg;
-            }
-
-            // 6. Положим A равным новому масиву, как если бы он был создан выражением new Array(len),
-            //    где Array является стандартным встроенным конструктором с этим именем,
-            //    а len является значением len.
-            A = new Array(len);
-
-            // 7. Положим k равным 0
-            k = 0;
-
-            // 8. Пока k < len, будем повторять
-            while (k < len) {
-
-                var kValue, mappedValue;
-
-                // a. Положим Pk равным ToString(k).
-                //   Это неявное преобразование для левостороннего операнда в операторе in
-                // b. Положим kPresent равным результату вызова внутреннего метода HasProperty
-                //    объекта O с аргументом Pk.
-                //   Этот шаг может быть объединён с шагом c
-                // c. Если kPresent равен true, то
-                if (k in O) {
-
-                    // i. Положим kValue равным результату вызова внутреннего метода Get
-                    //    объекта O с аргументом Pk.
-                    kValue = O[k];
-
-                    // ii. Положим mappedValue равным результату вызова внутреннего метода Call
-                    //     функции callback со значением T в качестве значения this и списком
-                    //     аргументов, содержащим kValue, k и O.
-                    mappedValue = callback.call(T, kValue, k, O);
-
-                    // iii. Вызовем внутренний метод DefineOwnProperty объекта A с аргументами
-                    // Pk, Описатель Свойства
-                    // { Value: mappedValue,
-                    //   Writable: true,
-                    //   Enumerable: true,
-                    //   Configurable: true }
-                    // и false.
-
-                    // В браузерах, поддерживающих Object.defineProperty, используем следующий код:
-                    // Object.defineProperty(A, k, {
-                    //   value: mappedValue,
-                    //   writable: true,
-                    //   enumerable: true,
-                    //   configurable: true
-                    // });
-
-                    // Для лучшей поддержки браузерами, используем следующий код:
-                    A[k] = mappedValue;
-                }
-                // d. Увеличим k на 1.
-                k++;
-            }
-
-            // 9. Вернём A.
-            return A;
-        };
+    try {
+        let res = myMap(data,item => item*2);
+        console.log(res);
+    } catch (e) {
+        console.log(e.message);
     }
 
+    //Моя версия map - end
 
-    // ДЗ - 1:
-    //
-    // написать аналоги методов для работы с массивами:
-    //
-    //     forEach, filter, map, slice, reduce, splice  пример:
-    //
-    //
-    //
-    //     let array = [1, 2, 3, 4, 5, 6];
-    //
-    // forEach(array, item => console.log(item));
-    //
-    // let greaterThan4 = filter(array, item => item > 4);
-    //
-    // let sqare = map(array, item => item*item);
-    //
-    //
-    //
-    // Описание того, как работают эти методы, есть на Mozilla Developer Network и в бесплатных видеоуроках LoftBlog/LoftSchool.
-    //
-    //
-    //
-    //     Реализация функции splice является задачей со звездочкой.
-    //
-    //     Ее выполнение не обязательно, но желательно.
-    //
-    //
-    //
-    //     Внимание:
-    //
-    // в данном задании запрещено использовать встроенные методы для работы с массивами! Разрешено использовать стандартные
-    //
-    // операторы 'for/for-in/while/if`' (и т.д.) и свойство 'length'
+    //Моя версия filter
+
+    let myFilter = (arr, callback) => {
+        let A = [],
+            j = 0;
+
+        if (typeof callback !== 'function') {
+            throw new Error(`${callback} это не функция!`);
+        }
+
+        for (let i = 0; i < arr.length; i++) {
+            if (callback(arr[i], i, arr)) {
+                A[j] = arr[i];
+                j++;
+            }
+        }
+
+        return A;
+    };
+    try {
+        let result = myFilter(data, item => item > 1);
+        console.log(result);
+    } catch (e) {
+        console.log(e.message);
+    }
+
+    //Моя версия filter - end
+
+    //Моя версия slice
+
+    let mySlice = (arr, start = 0, end = arr.length) => {
+
+        if (!arr.length) {
+            throw new Error('Передайте не пустой массив!');
+        }
+
+        if (start >= arr.length) return [];
+
+        if (Math.sign(start) === -1) start+=arr.length;
+
+        if (Math.sign(end) === -1) end+=arr.length;
+
+        let A = [],
+            j = 0;
+
+        for (let i = start; i < end; i++) {
+            A[j] = arr[i];
+            j++;
+        }
+
+        return A;
+    };
+    try {
+        let arr = mySlice(data,1,3);
+        console.log(arr);
+    } catch (e) {
+        console.log(e.message);
+    }
+
+    //Моя версия slice - end
+
+    //Моя версия reduce
+
+    let myReduce = (arr, callback, initialValue) => {
+
+        let i = 0;
+
+        if (arr.length === 0 && !initialValue) {
+            throw new Error(`Переданный массив ПУСТ и начальное значение не задано! Нам не с чем работать!`);
+        }
+
+        if (arr.length === 1 && !initialValue) {
+            return arr[0];
+        }
+
+        if (arr.length === 0 && initialValue) {
+            return initialValue;
+        }
+
+        if (typeof callback !== "function") {
+            throw new Error(`${callback} это не функция!`);
+        }
+
+        let previusValue, currentValue;
+
+        if (initialValue) {
+            previusValue=initialValue;
+
+            for (i; i < arr.length; i++) {
+                previusValue= callback(previusValue,currentValue = arr[i],i,arr);
+            }
+
+        } else {
+            previusValue = arr[0];
+
+            for (i = 1; i < arr.length; i++) {
+                previusValue = callback(previusValue,currentValue = arr[i],i, arr);
+            }
+        }
 
 
+        return previusValue;
+    };
 
+    //
+    try{
+        let result = myReduce(data, (sum, cur) => {
+            return sum + cur;
+        });
+
+        console.log(result);
+    } catch (e) {
+        console.log(e.message);
+    }
+
+    //Моя версия reduce - end
+
+    // * Моя версия splice
+
+    function mySplice (arr, start, deleteCount){
+
+        let result = [],
+            element,
+            max = Math.max,
+            min = Math.min,
+            len = arr.length,
+            delta,
+            k = 0,
+            insertCount = max(arguments.length - 3, 0),
+            shift_count,
+            new_len;
+
+        start = start || 0;
+
+        if (start < 0) {
+            start += len;
+        }
+
+        start = max(min(start, len), 0);
+
+        deleteCount = max(min(typeof deleteCount === 'number' ?
+        deleteCount : len, len - start), 0);
+
+        delta = insertCount - deleteCount;
+
+        new_len = len + delta;
+
+        while (k < deleteCount) {
+            element = arr[start + k];
+            if (element !== undefined) {
+                result[k] = element;
+            }
+            k+=1;
+        }
+
+        shift_count = len - start - deleteCount;
+
+        if (delta < 0) {
+            k = start + insertCount;
+            while (shift_count) {
+                arr[k] = arr[k - delta];
+                k+=1;
+                shift_count -= 1;
+            }
+            arr.length = new_len;
+        } else if (delta > 0) {
+            k = 1;
+            while (shift_count) {
+                arr[new_len - k] = arr[len - k];
+                k+=1;
+                shift_count -=1;
+            }
+            arr.length = new_len;
+        }
+
+        for (k = 0; k < insertCount; k+=1) {
+            arr[start + k] = arguments[k + 3];
+        }
+
+        return result;
+
+    }
+
+    let ddd = [1,2,3,4,5,6];
+
+    let a = mySplice(ddd,1,0);
+    console.log(ddd);
+    console.log(a);
+    // * Моя версия splice
 })();
