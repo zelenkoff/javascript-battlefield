431bab67d5cbb2c7303efd9c1f0fc912bea52b0d
Evgeniy Zelenkov <skyydail@gmail.com>
2016-11-21 16:53:33 +0300

    ht4_4 DOM parser - complete

--- ht4_4/index.js
+++ ht4_4/index.js
@@ -17,49 +17,56 @@
 //     Для продвинутой работы с классами элемента, рекомендуется ознакомиться со свойством `classList`.
 
 (() => {
-    let begin = document.querySelector('body');
+    let begin = document.querySelector('body'),
+        elements = {};
 
     let scanDOM = (start) => {
-            let textNode = 0;
+        let nodes = start.childNodes,
+            len = nodes.length;
 
-        for (let i = 0; i < start.childNodes.length; i++) {
-            if (start.childNodes[i].nodeType === 3) {
-                console.log(start.childNodes[i]);
-                textNode++;
-            } else if (start.childNodes[i].nodeType === 1
-                && start.childNodes[i].length > 0) {
-                scanDOM(start.childNodes[i]);
+        for (let i = 0; i < len; i++) {
+
+            if (nodes[i].nodeType === 3) {
+
+                if (elements["Текстовых узлов"] === undefined) {
+                    elements["Текстовых узлов"] = 1;
+                } else {
+                    elements["Текстовых узлов"] += 1;
+                }
             }
 
-            if (start.childNodes[i] = start.lastChild) {
-                console.log('Текстовых узлов: ', textNode);
+            if (nodes[i].nodeType === 1) {
+
+                if (nodes[i].classList.length > 1) {
+                    for (let j = 0; j < nodes[i].classList.length; j++) {
+                        elements["Элементов с классом: " + nodes[i].classList[j]]
+                            = elements["Элементов с классом: " + nodes[i].classList[j]] !== undefined
+                            ? elements["Элементов с классом: " + nodes[i].classList[j]] += 1
+                            : elements["Элементов с классом: " + nodes[i].classList[j]] = 1;
+                    }
+                } else {
+                    if (elements["Элементов с классом: " + nodes[i].classList.value] === undefined) {
+                        elements["Элементов с классом: " + nodes[i].classList.value] = 1;
+                    } else {
+                        elements["Элементов с классом: " + nodes[i].classList.value] +=1;
+                    }
+                }
+
+                if (elements["Тегов " + nodes[i].tagName] === undefined) {
+                    elements["Тегов " + nodes[i].tagName] = 1;
+                } else {
+                    elements["Тегов " + nodes[i].tagName] += 1;
+                }
+
+                scanDOM(nodes[i]);
             }
-            // if (i.nodeType === 3) {
-            //     console.log(i);
-            //     textNode++;
-            // } else if (i.nodeType === 1 && i.childNodes.length > 0) {
-            //     scanDOM(i);
-            // }
-            //
-            // if (i.lastChild === i) {
-            //     console.log('Текстовых узлов: ', textNode);
-            // }
         }
-
-
-        // for (let i of start.childNodes) {
-        //
-        //     if (i.nodeType === 3) {
-        //         console.log(i);
-        //         textNode++;
-        //     } else if (i.nodeType === 1 && i.childNodes.length > 0) {
-        //         scanDOM(i);
-        //     }
-        // }
-        // console.log('Текстовых узлов: ', textNode);
     };
-
     scanDOM(begin);
 
+    for (let i in elements) {
+
+        console.log(`${i} -> ${elements[i]}`);
+    }
 
 })();
