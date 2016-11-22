a38d357c8dd70272ecce0d9fc58fda889d3bad1e
Evgeniy Zelenkov <skyydail@gmail.com>
2016-11-22 17:57:26 +0300

    ht5_2 complete

---
+++ ht5_2/index.js
@@ -0,0 +1,66 @@
+// Создать страницу с кнопкой. При клике на кнопку,
+//     на странице должен создаваться div произвольных размеров, в произвольном месте.
+//     Цвет фона div'а должен быть каждый раз случайным.
+// Созданные div'ы можно перетаскивать мышкой (drag & drop)
+
+(() => {
+
+    let button = document.querySelector('.creator'),
+        body = document.querySelector('body');
+
+    let randomColor = () => {
+        let r = Math.round(Math.random() * 256),
+            g = Math.round(Math.random() * 256),
+            b = Math.round(Math.random() * 256);
+
+
+        return result = '#' + r.toString(16) + g.toString(16) + b.toString(16);
+    };
+
+    body.addEventListener('mousedown', e => {
+
+        let element = e.target;
+
+        if (element.classList.value !== 'creator') {
+               element.style.position = 'absolute';
+                moveAt(e);
+                element.style.zIndex = 1000;
+
+                function moveAt(e) {
+                    element.style.left = e.pageX - element.offsetWidth / 2 + 'px';
+                    element.style.top = e.pageY - element.offsetWidth / 2 + 'px';
+                }
+
+                document.onmousemove = function (e) {
+                    moveAt(e);
+                };
+
+                element.onmouseup = () => {
+                  document.onmousemove = null;
+                    element.onmouseup = null;
+                };
+        }
+    });
+
+    button.addEventListener('click', e => {
+
+        let div = document.createElement('div'),
+            text = document.createTextNode('DIV');
+
+        div.style.width = Math.random() * 100 + 'px';
+        div.style.height = Math.random() * 100 + 'px';
+
+        div.appendChild(text);
+
+        body.appendChild(div);
+
+        div.style.position = 'absolute';
+        div.style.left = Math.random() * window.innerWidth * Math.random() + 'px';
+        div.style.top = Math.random() * window.innerHeight * Math.random() + 'px';
+        div.style.background = randomColor();
+        div.style.border = '1px solid';
+        div.style.cursor = 'pointer';
+
+    });
+
+})();
