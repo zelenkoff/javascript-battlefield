cb87a0395515f7adc800ff6e04bb9d38d8a6a07b
Evgeniy Zelenkov <skyydail@gmail.com>
2016-12-28 15:40:27 +0300

    initial DONE commit

---
+++ ht11_1/controller.js
@@ -0,0 +1,23 @@
+var Controller = {
+    musicRoute: function() {
+        return Model.getMusic().then(function(music) {
+            results.innerHTML = View.render('music', {list: music});
+        });
+    },
+    friendsRoute: function() {
+        return Model.getFriends().then(function(friends) {
+            results.innerHTML = View.render('friends', {list: friends});
+        });
+    },
+    newsRoute: function() {
+        return Model.getNews().then(function (news) {
+            console.log(news);
+            results.innerHTML = View.render('news', {list: news.items});
+        });
+    },
+    groupRoute: function () {
+        return Model.getGroups().then(function (groups) {
+            results.innerHTML = View.render('groups', {list: groups.items});
+        })
+    }
+};
